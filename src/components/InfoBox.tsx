b61813de892c6d152cbf005384191d89ee9cc0f9
Reski Abbas <reskiabbas@student.unismuh.ac.id>
2026-02-11 22:00:08 +0700

    updated

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -2,10 +2,11 @@ import { type ReactNode } from "react";
 
 type InfoBoxProps = {
   mode: "hint" | "warning";
+  severity?: "low" | "medium" | "high";
   children: ReactNode;
 };
 
-function InfoBox({ mode, children }: InfoBoxProps) {
+function InfoBox({ mode, severity, children }: InfoBoxProps) {
   if (mode === "hint") {
     return (
       <aside className="infobox infobox-hint">
@@ -15,7 +16,7 @@ function InfoBox({ mode, children }: InfoBoxProps) {
   }
 
   return (
-    <aside className="infobox infobox-warning warning--medium ">
+    <aside className={`infobox infobox-warning warning--${severity}`}>
       {mode === "warning" ? <h2>Warning</h2> : null}
       <p>{children}</p>
     </aside>
